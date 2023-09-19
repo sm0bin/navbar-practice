936333699d5d88f7b4a518a2106e8a8f71b5272a
GhostMobin <s1810677110@ru.ac.bd>
2023-09-19 23:07:44 +0600

    responsive nav added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,9 @@
-import { HiMenu } from "react-icons/hi";
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   const navLinks = [
     { text: "Home", url: "/" },
     { text: "Products", url: "/products" },
@@ -14,7 +17,11 @@ const Header = () => {
       <a href="" className="font-bold text-3xl">
         TechHub
       </a>
-      <ul className=" gap-12 items-center text-lg hidden lg:flex">
+      <ul
+        className={`duration-700 gap-12 items-center text-lg lg:flex lg:static absolute right-6 md:right-10 text-right p-6 rounded-xl max-lg:space-y-4 min-[0px]:max-lg:bg-slate-300 min-[0px]:max-lg:dark:bg-slate-600 ${
+          navOpen ? "top-20" : "-top-64"
+        }`}
+      >
         {navLinks.map((link, id) => (
           <li key={id}>
             <a href="#">{link.text}</a>
@@ -26,8 +33,11 @@ const Header = () => {
         Explore Now
       </button>
 
-      <button className="text-3xl border px-2 py-1 rounded-md lg:hidden">
-        <HiMenu />
+      <button
+        className="text-3xl border px-2 py-1 rounded-md lg:hidden"
+        onClick={() => setNavOpen(!navOpen)}
+      >
+        {navOpen ? <HiX /> : <HiMenu />}
       </button>
     </nav>
   );
