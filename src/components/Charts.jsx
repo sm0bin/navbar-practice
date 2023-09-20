68176bf028c0efa2ba320ecf00840e5c509909e2
GhostMobin <s1810677110@ru.ac.bd>
2023-09-20 22:31:39 +0600

    readme

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -4,7 +4,7 @@ import StackedBarChart from "./StackedBarChart";
 
 const Charts = () => {
   return (
-    <div className="flex gap-6  max-w-screen-2xl mx-6 md:mx-10 2xl:mx-auto my-10">
+    <div className="lg:flex gap-6  max-w-screen-2xl mx-6 md:mx-10 2xl:mx-auto my-10">
       <SimpleLineChart></SimpleLineChart>
       <StackedBarChart></StackedBarChart>
     </div>
